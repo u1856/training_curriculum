c015fe9379a560d88a1ede903aef23a9eb87f497
u1856 <sglspy856@gmail.com>
2022-06-06 21:01:07 +0900

    日付にあった曜日が表示されるよう修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -26,6 +26,7 @@ class CalendarsController < ApplicationController
 
     @todays_date = Date.today
     @week_date = Date.today.wday 
+    
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
@@ -38,12 +39,12 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = @week_date  # wdayメソッドを用いて取得した数値
+      wday_num = @week_date + x  # wdayメソッドを用いて取得した数値
       if wday_num >= 7  #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wdays: wdays[x]}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wdays: wdays[wday_num]}
       @week_days.push(days)
     end
 
